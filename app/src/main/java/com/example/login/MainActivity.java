dd273a031e7facece1f1a24c921df86e5d977e89
Jose_Luis <joselu2004.jlrc@gmail.com>
2023-11-23 22:12:44 +0100

    login y registro hecho

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,36 +1,52 @@
 package com.example.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
-import java.util.Locale;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText met_user;
     private EditText met_password;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        met_user=(EditText) findViewById(R.id.editTextUsername);
-        met_password=(EditText) findViewById(R.id.editTextPassword);
+        met_user = findViewById(R.id.editTextUsername);
+        met_password = findViewById(R.id.editTextPassword);
     }
 
-    public void Translate(View view){
-        
+    public void Translate(View view) {
+        // Implementa la función de traducción si es necesario
     }
 
-    public void Register(View view){
+    public void Register(View view) {
         Intent i = new Intent(this, Registrer.class);
         startActivity(i);
+    }
+
+    public void Login(View view) {
+        String inputUsername = met_user.getText().toString();
+        String inputPassword = met_password.getText().toString();
+
+        SharedPreferences preferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        String savedUsername = preferences.getString("nombre_usuario", "");
+        String savedPassword = preferences.getString("contrasena", "");
+
+        if (inputUsername.equals(savedUsername) && inputPassword.equals(savedPassword)) {
+            Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+            // Puedes redirigir a la siguiente actividad o realizar otras acciones después del inicio de sesión exitoso
+            Intent i = new Intent(this, MainActivity2.class);
+            startActivity(i);
+
+        } else {
+            Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+        }
     }
 }
