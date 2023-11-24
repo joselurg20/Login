67cf643eb8244f637a06e25e79be084d7edf7590
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-11-24 12:12:13 +0100

    Cambios en login

--- app/src/main/java/com/example/login/HomeActivity.java
+++ app/src/main/java/com/example/login/HomeActivity.java
@@ -1,4 +1,22 @@
 package com.example.login;
 
-public class HomeActivity {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView msgBienvenida;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        msgBienvenida = findViewById(R.id.editTextUsername);
+        msgBienvenida.setText("Bienvenido: "+getIntent().getStringExtra("Nombre"));
+    }
 }
