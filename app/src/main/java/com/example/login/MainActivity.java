b6da40452371396719cff44646d63e77fc0b313f
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-11-24 10:11:58 +0100

    Login correcto y pantalla de home creada

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -3,30 +3,31 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
-import java.util.Locale;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText met_user;
     private EditText met_password;
-
+    private TextView msg_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        met_user=(EditText) findViewById(R.id.editTextUsername);
-        met_password=(EditText) findViewById(R.id.editTextPassword);
+        met_user=findViewById(R.id.editTextUsername);
+        met_password=findViewById(R.id.editTextPassword);
     }
 
-    public void Translate(View view){
-        
+    public void logIn(View view){
+        Intent i = new Intent(this, Registrer.class);
+
+
     }
 
     public void Register(View view){
